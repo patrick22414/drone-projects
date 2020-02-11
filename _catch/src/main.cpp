e6a417382729186a2998de3eefbd049db177236b
Patrick Yang <patrick22414@outlook.com>
2020-02-11 12:09:50 -0700

    Using cxxopts

--- _catch/src/main.cpp
+++ _catch/src/main.cpp
@@ -10,6 +10,7 @@
 #include <thread>
 #include <optional>
 
+#include "cxxopts.hpp"
 #include "internal.h"
 
 using namespace cv;
@@ -18,8 +19,7 @@ using namespace std;
 using namespace this_thread;
 using namespace chrono;
 
-
-optional<tuple<Vec2d, double, steady_clock::time_point>> getBallInImage(VideoCapture &v)
+optional<tuple<Vec2d, double, steady_clock::time_point>> getBallInImage(VideoCapture& v)
 {
     /* Get ball position, radius, and timestamp in Image frame
      * Parameters:
@@ -63,10 +63,10 @@ optional<tuple<Vec2d, double, steady_clock::time_point>> getBallInImage(VideoCap
         auto m = moments(largestContour);
 
         double area = m.m00;
-        double xi = 0;
-        double yi = 0;
-        double ri = 0;
-        if (area > 20) {
+        double xi   = 0;
+        double yi   = 0;
+        double ri   = 0;
+        if (area > 25) {
             xi = m.m10 / area;
             yi = m.m01 / area;
             ri = sqrt(area / CV_PI);
@@ -80,15 +80,14 @@ optional<tuple<Vec2d, double, steady_clock::time_point>> getBallInImage(VideoCap
     return {};
 }
 
-
-Vec3d cameraTransform(const Vec2d &ball_position_px, double ball_radius_px, const Vec2d &resolution)
+Vec3d cameraTransform(const Vec2d& ball_position_px, double ball_radius_px, const Vec2i& resolution)
 {
     const double ball_radius = 3.3e-2; // tennis ball radius 3.3 cm
 
     // Pi Camera V1 parameters
-    const double sensor_w_px = 2592; // sensor width in pixels
-    const double sensor_h_px = 1944; // sensor height in pixels
-    const double focal_length = 3.6e-3; // focal length 3.6mm
+    const double sensor_w_px     = 2592;   // sensor width in pixels
+    const double sensor_h_px     = 1944;   // sensor height in pixels
+    const double focal_length    = 3.6e-3; // focal length 3.6mm
     const double real_pixel_size = 1.4e-6; // real pixel size 1.4um
 
     double res_w_px = resolution[0];
@@ -101,9 +100,9 @@ Vec3d cameraTransform(const Vec2d &ball_position_px, double ball_radius_px, cons
 
     // camera intrinsic matrix
     double intrinsics[3][3] = {
-            {focal_length, 0,            0.5 * (res_w_px - 1) * pix_size},
-            {0,            focal_length, 0.5 * (res_h_px - 1) * pix_size},
-            {0,            0,            1},
+        {focal_length, 0, 0.5 * (res_w_px - 1) * pix_size},
+        {0, focal_length, 0.5 * (res_h_px - 1) * pix_size},
+        {0, 0, 1},
     };
 
     // object homogeneous coordinates, Image frame (z = 1)
@@ -133,30 +132,26 @@ Vec3d cameraTransform(const Vec2d &ball_position_px, double ball_radius_px, cons
     return {xc, yc, zc};
 }
 
-
-Vec3d worldTransform(const Vec3d &p_c, const Telemetry::PositionNED &t, const Telemetry::EulerAngle &r)
+Vec3d worldTransform(const Vec3d& p_c, const Telemetry::PositionNED& t, const Telemetry::EulerAngle& r)
 {
     // TODO: given a point in the Camera frame, transform the position into the World frame
 
-    Matx33d drone_attitude_rotation = eulerAngleToRotationMatrix(r);
+    Matx33d drone_attitude_rotation  = eulerAngleToRotationMatrix(r);
     Matx33d camera_mounting_rotation = eulerAngleToRotationMatrix({0, 180, 90 + 20});
 
     // Total extrinsic rotation
     Matx33d A = drone_attitude_rotation * camera_mounting_rotation;
-//    Matx33d A = drone_attitude_rotation;
+    //    Matx33d A = drone_attitude_rotation;
 
     //
-    Matx31d b(
-            p_c[0] + t.north_m, p_c[1] + t.east_m, p_c[2] + t.down_m
-    );
+    Matx31d b(p_c[0] + t.north_m, p_c[1] + t.east_m, p_c[2] + t.down_m);
 
     Matx31d x = A.solve(b, DECOMP_SVD);
 
     return Vec3d(x.val);
 }
 
-
-Vec6d calculateParabola(const vector<Vec3d> &points, const vector<nanoseconds> &timestamps)
+Vec6d calculateParabola(const vector<Vec3d>& points, const vector<nanoseconds>& timestamps)
 {
     // Find the parameters of a parabola from a series of points in World frame
     assert(points.size() == timestamps.size());
@@ -168,11 +163,11 @@ Vec6d calculateParabola(const vector<Vec3d> &points, const vector<nanoseconds> &
     for (int i = 0; i < n; ++i) {
         double s = 1e-9 * timestamps[i].count(); // time in second
 
-        int A_start = i * 3 * 6;
+        int A_start         = i * 3 * 6;
         A_data[A_start + 0] = A_data[A_start + 7] = A_data[A_start + 14] = s;
         A_data[A_start + 3] = A_data[A_start + 10] = A_data[A_start + 17] = 1;
 
-        int b_start = i * 3;
+        int b_start         = i * 3;
         b_data[b_start + 0] = points[i][0];
         b_data[b_start + 1] = points[i][1];
         b_data[b_start + 2] = points[i][2] - G_HALF * s * s;
@@ -184,13 +179,12 @@ Vec6d calculateParabola(const vector<Vec3d> &points, const vector<nanoseconds> &
 
     solve(A, b, x, DECOMP_SVD);
 
-    Vec6d parabola((double *) x.data);
+    Vec6d parabola((double*)x.data);
 
     return parabola;
 }
 
-
-Offboard::PositionNEDYaw calculateDestination(const Vec6d &parabola, double catch_alt)
+Offboard::PositionNEDYaw calculateDestination(const Vec6d& parabola, double catch_alt)
 {
     // catch altitude should be negated in NED frame
     catch_alt = -catch_alt;
@@ -221,56 +215,51 @@ Offboard::PositionNEDYaw calculateDestination(const Vec6d &parabola, double catc
     double catch_x = vx * t + x0;
     double catch_y = vy * t + y0;
 
-    return {(float) catch_x, (float) catch_y, (float) catch_alt, 0};
+    return {(float)catch_x, (float)catch_y, (float)catch_alt, 0};
 }
 
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    const string keys =
-            "{h help ?       |    | Print this message}"
-            "{v video        |    | Use a video file instead of camera}"
-            "{n n-frames     | 10 | Maximum number of frames used to calculate the trajectory of the ball}"
-            "{r resolution   | s  | Camera resolution. Valid values are s: 640x480; m: 1296x972; l: 2592x1944}"
-            "{a catch-alt    | -1 | Altitude used by the drone to catch the ball. Default -1 will remain at current altitude}"
-            "{ta takeoff-alt | 3  | Takeoff altitude}"
-            "{@connection    | serial:///dev/serial0:921600 | MAVLink connection url}";
+    // clang-format off
+    cxxopts::Options options("catch", "The main program that will catch the ball");
+    options.add_options()
+        ("h,help", "Print help message")
+        ("v,video", "Use a video file instead of the camera for testing", cxxopts::value<string>())
+        ("n,n-frames","Maximum number of frames used to calculate the trajectory of the ball", cxxopts::value<int>()->default_value("60"))
+        ("r,resolution","Camera resolution. One of 's': 640x480; 'm': 1296x972; 'l': 2592x1944",cxxopts::value<char>()->default_value("s"))
+        ("c,catch-alt", "Altitude used to catch the ball",cxxopts::value<double>()->default_value("3"))
+        ("t,takeoff-alt", "Altitude used for takeoff", cxxopts::value<double>()->default_value("3"))
+        ("connection", "MAVSDK connection url", cxxopts::value<string>()->default_value("serial:///dev/serial0:921600"));
+    options.parse_positional({"connection"});
+    // clang-format on
 
-    CommandLineParser parser(argc, argv, keys);
-    parser.about("The main program that will catch the ball");
+    auto args = options.parse(argc, argv);
 
-    if (parser.has("help")) {
-        parser.printMessage();
+    auto video_file  = args.count("video") ? args["video"].as<string>() : "";
+    auto n_frames    = args["n-frames"].as<int>();
+    auto res_char    = args["resolution"].as<char>();
+    auto catch_alt   = args["catch-alt"].as<double>();
+    auto takeoff_alt = args["takeoff-alt"].as<double>();
+    auto connection  = args["connection"].as<string>();
+
+    if (args.count("help")) {
+        cout << options.help() << endl;
         return 0;
     }
 
-    // Parse and check arguments
-    auto video_path = parser.get<string>("video");
-    auto n_frames = parser.get<int>("n-frames");
-    auto catch_alt = parser.get<int>("catch-alt");
-    auto takeoff_alt = parser.get<float>("takeoff-alt");
-    auto res_char = parser.get<string>("resolution")[0];
-    auto connection = parser.get<string>("@connection");
-
-    if (!parser.check()) {
-        parser.printErrors();
-        exit(-1);
-    }
-
-    if (n_frames < 8) {
-        cerr << CLI_COLOR_RED << "Invalid n-frames `" << n_frames << "`. Must be at least 8"
-             << CLI_COLOR_NORMAL << endl;
+    if (n_frames < 15) {
+        cerr << CLI_COLOR_RED << "Invalid n-frames `" << n_frames << "`. Must be at least 15" << CLI_COLOR_NORMAL
+             << endl;
         exit(-1);
     }
 
     if (catch_alt < 3 && catch_alt != -1) {
         cerr << CLI_COLOR_RED << "Invalid altitude `" << catch_alt
-             << "`. Must be at least 3 meters or -1 (use current altitude)" << CLI_COLOR_NORMAL
-             << endl;
+             << "`. Must be at least 3 meters or -1 (use current altitude)" << CLI_COLOR_NORMAL << endl;
         exit(-1);
     }
 
-    Vec2d resolution;
+    Vec2i resolution;
     if (res_char == 's')
         resolution = {640, 480};
     else if (res_char == 'm')
@@ -278,20 +267,16 @@ int main(int argc, char *argv[])
     else if (res_char == 'l')
         resolution = {2592, 1944};
     else {
-        cerr << CLI_COLOR_RED << "Invalid resolution `" << res_char << "`. Must be one of s/m/l"
-             << CLI_COLOR_NORMAL << endl;
+        cerr << CLI_COLOR_RED << "Invalid resolution `" << res_char << "`. Must be one of s/m/l" << CLI_COLOR_NORMAL
+             << endl;
         exit(-1);
     }
 
     // Prepare VideoCapture
     VideoCapture v;
-    if (parser.has("video")) {
-        cout << "Using video file " << video_path << endl;
-        v = VideoCapture(video_path);
-
-        if (parser.has("resolution")) {
-            cout << "Ignoring resolution setting" << endl;
-        }
+    if (args.count("video")) {
+        cout << "Using video file " << video_file << endl;
+        v = VideoCapture(video_file);
     } else {
         cout << "Using camera with resolution " << res_char << ": " << resolution << endl;
         v = VideoCapture(0, CAP_V4L2);
@@ -305,13 +290,11 @@ int main(int argc, char *argv[])
     }
 
     // Test VideoCapture
-    Mat test_image;
-    if (!v.read(test_image)) {
+    Mat first_frame;
+    if (!v.read(first_frame)) {
         cerr << CLI_COLOR_RED << "Cannot read frame" << CLI_COLOR_NORMAL << endl;
         exit(-1);
     }
-
-    test_image.release();
 
 #ifdef USE_DRONE
     // Prepare Drone
@@ -321,18 +304,16 @@ int main(int argc, char *argv[])
 
     // Wait for system to connect via heartbeat
     while (!mav.is_connected()) {
-        cout << CLI_COLOR_YELLOW << "Waiting for system to connect via heartbeat"
-             << CLI_COLOR_NORMAL << endl;
+        cout << CLI_COLOR_YELLOW << "Waiting for system to connect via heartbeat" << CLI_COLOR_NORMAL << endl;
         sleep_for(seconds(1));
     }
 
-    System &system = mav.system();
-    cout << CLI_COLOR_GREEN << "System connected: " << system.get_uuid() << CLI_COLOR_NORMAL
-         << endl;
+    System& system = mav.system();
+    cout << CLI_COLOR_GREEN << "System connected: " << system.get_uuid() << CLI_COLOR_NORMAL << endl;
 
     // Make system objects
-    auto action = Action(system);
-    auto offboard = Offboard(system);
+    auto action    = Action(system);
+    auto offboard  = Offboard(system);
     auto telemetry = Telemetry(system);
 
 #if 0
@@ -369,10 +350,12 @@ int main(int argc, char *argv[])
         auto result = getBallInImage(v);
 
         if (result.has_value()) {
-            cout << "\nFound ball in image" << endl;
+            cout << "Found ball in image" << endl;
             break;
-        } else {
-            cout << CLI_COLOR_YELLOW << "\rWaiting for ball to appear in image" << CLI_COLOR_NORMAL;
+        } else if (!args.count("video")) {
+            // Print this message only when using a camera
+            cout << CLI_COLOR_YELLOW << "Waiting for ball to appear in image" << CLI_COLOR_NORMAL << endl;
+            sleep_for(milliseconds(100));
         }
     }
 
@@ -391,15 +374,15 @@ int main(int argc, char *argv[])
             drone_positions.push_back(telemetry.position_velocity_ned().position);
             drone_rotations.push_back(telemetry.attitude_euler_angle());
 #else
-            drone_positions.push_back({0, 0, takeoff_alt});
+            drone_positions.push_back({0, 0, (float)takeoff_alt});
             drone_rotations.push_back({0, 0, 0});
 #endif // USE_DRONE
             auto values = result.value();
             positions_i.push_back(get<0>(values));
             radii_i.push_back(get<1>(values));
 
-            if (parser.has("video")) {
-                timestamps.emplace_back((int) 1e9 / 30 * (i + 1)); // assume a 30FPS video
+            if (args.count("video")) {
+                timestamps.emplace_back((int)1e9 / 30 * (i + 1)); // assume a 30FPS video
             } else {
                 timestamps.push_back(duration_cast<nanoseconds>(get<2>(values) - t0));
             }
@@ -409,18 +392,24 @@ int main(int argc, char *argv[])
     v.release();
 
     cout << "Positions acquired in " << positions_i.size() << " frames" << endl;
-    cout << "Total interval: "
-         << duration_cast<milliseconds>(timestamps[timestamps.size() - 1] - timestamps[0]).count()
+    cout << "Total interval: " << duration_cast<milliseconds>(timestamps[timestamps.size() - 1] - timestamps[0]).count()
          << "ms" << endl;
 
     // A series of coordinate transforms
     vector<Vec3d> positions_c;
-    for (int i = 0; i < positions_i.size(); ++i) {
-        positions_c.push_back(cameraTransform(positions_i[i], radii_i[i], resolution));
+    if (args.count("video")) {
+        cout << "Ignoring camera resolution setting when using a video" << endl;
+        for (int i = 0; i < positions_i.size(); ++i) {
+            positions_c.push_back(cameraTransform(positions_i[i], radii_i[i], {first_frame.cols, first_frame.rows}));
+        }
+    } else {
+        for (int i = 0; i < positions_i.size(); ++i) {
+            positions_c.push_back(cameraTransform(positions_i[i], radii_i[i], resolution));
+        }
     }
 
-    cout << "Got the following positions in Camera frame:" << endl;
-    for (const auto &p: positions_c) {
+    cout << "Got following positions in Camera frame:" << endl;
+    for (const auto& p : positions_c) {
         cout << "\t" << p << "," << endl;
     }
 
@@ -429,8 +418,8 @@ int main(int argc, char *argv[])
         positions_w.push_back(worldTransform(positions_c[i], drone_positions[i], drone_rotations[i]));
     }
 
-    cout << "Got the following positions in World frame:" << endl;
-    for (const auto &p: positions_w) {
+    cout << "Got following positions in World frame:" << endl;
+    for (const auto& p : positions_w) {
         cout << "\t" << p << "," << endl;
     }
 
@@ -444,8 +433,12 @@ int main(int argc, char *argv[])
 #endif
 
     auto destination = calculateDestination(parabola, catch_alt);
-    cout << CLI_COLOR_GREEN << "Will catch the ball at " << destination << CLI_COLOR_NORMAL << endl;
+    cout << CLI_COLOR_GREEN << "Destination is at " << destination << CLI_COLOR_NORMAL << endl;
 
+    if (abs(destination.north_m) > 10 || abs(destination.east_m) > 10) {
+        cout << CLI_COLOR_RED << "Destination too far. Aborting for safety" << CLI_COLOR_NORMAL << endl;
+        exit(0);
+    }
 
 #ifdef USE_DRONE
     // Fly to destination in offboard mode
