6706975eb9278590070eb4d3bbb5b0b84d31db77
Patrick Yang <patrick22414@outlook.com>
2020-02-11 15:30:22 -0700

    Add capture_video script

---
+++ capture_video.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+FLY_MODE=air
+if [ -n "$1" ]; then
+	echo "Using fly mode suffix:" $1
+	FLY_MODE=$1
+fi
+
+i=1
+
+while [ -f "$HOME/Videos/v$i-$FLY_MODE.mp4" ]; do
+	i=$((i + 1))
+done
+
+FILE_H264=$HOME/Videos/v$i-$FLY_MODE.h264
+FILE_MP4=$HOME/Videos/v$i-$FLY_MODE.mp4
+
+if [ -n "$AWB" ]; then
+	raspivid -t 5000 -w 640 -h 480 -awb sun -fps 30 -o $FILE_H264
+else
+	raspivid -t 5000 -w 640 -h 480          -fps 30 -o $FILE_H264
+fi
+
+MP4Box -add $FILE_H264 -fps 30 $FILE_MP4
+
+rm $FILE_H264
