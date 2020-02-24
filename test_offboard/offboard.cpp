d5547495a701db25175715b9b1a24e491d2758b2
Patrick Yang <patrick22414@outlook.com>
2020-02-24 11:03:23 -0700

    Add recording thread

--- test_offboard/offboard.cpp
+++ test_offboard/offboard.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/opencv.hpp>
 
 #include <chrono>
+#include <filesystem>
 #include <thread>
 
 #define CONSOLE_TEXT_ERROR "\033[31m"     // Turn text on console red
@@ -12,6 +13,11 @@
 #define CONSOLE_TEXT_NORMAL "\033[0m"     // Restore normal console colour
 
 using namespace mavsdk;
+
+using std::this_thread::sleep_for;
+using std::chrono::milliseconds;
+
+namespace fs = std::filesystem;
 
 // Handles Action's result
 inline void check_action_result(Action::Result result, const std::string& message)
@@ -40,8 +46,66 @@ inline void check_connection_result(ConnectionResult result, const std::string& 
     }
 }
 
+static bool is_recording = true;
+
+std::string generate_video_filename(const std::string& prefix = "test")
+{
+    fs::path full_filename = fs::path(std::getenv("HOME")) / "Videos";
+    std::stringstream filename;
+    for (int i = 1;; ++i) {
+        filename.str("");
+        filename << prefix << "-v" << i << ".mp4";
+
+        if (!fs::exists(fs::path(full_filename) / filename.str())) {
+            break;
+        }
+    }
+
+    std::cout << "Using video filename " << fs::path(full_filename) / filename.str() << std::endl;
+
+    return fs::path(full_filename) / filename.str();
+}
+
+void start_recording()
+{
+    cv::VideoCapture capture(0, cv::CAP_V4L2);
+
+    capture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+
+    if (!capture.isOpened()) {
+        std::cerr << CONSOLE_TEXT_ERROR << "Cannot open camera capture" << CONSOLE_TEXT_NORMAL << std::endl;
+        return;
+    }
+
+    cv::Mat frame;
+    if (!capture.read(frame)) {
+        std::cerr << CONSOLE_TEXT_ERROR << "Cannot read frame or end of video" << CONSOLE_TEXT_NORMAL << std::endl;
+        return;
+    }
+
+    auto filename = generate_video_filename();
+    auto fps      = 30.0;
+    auto codec    = cv::VideoWriter::fourcc('m', 'p', '4', 'v');
+    cv::VideoWriter writer(filename, codec, fps, frame.size());
+
+    std::cout << "Start recording!!!" << std::endl;
+    while (is_recording) {
+        if (!capture.read(frame)) {
+            std::cerr << CONSOLE_TEXT_ERROR << "Cannot read frame or end of video" << CONSOLE_TEXT_NORMAL << std::endl;
+            return;
+        }
+
+        writer.write(frame);
+    }
+
+    std::cout << "Stop recording!!!" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
+    std::thread recording_thread(start_recording);
+
     Mavsdk dc;
     std::string connection_url;
     ConnectionResult connection_result;
@@ -64,7 +128,7 @@ int main(int argc, char** argv)
     // Wait for the system to connect via heartbeat
     while (!dc.is_connected()) {
         std::cout << "Wait for system to connect via heartbeat" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        sleep_for(milliseconds(1000));
     }
 
     // System got discovered.
@@ -76,7 +140,7 @@ int main(int argc, char** argv)
 
     while (!telemetry.health_all_ok()) {
         std::cout << "Waiting for system to be ready" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        sleep_for(milliseconds(1000));
     }
 
     std::cout << "System is ready" << std::endl;
@@ -89,70 +153,70 @@ int main(int argc, char** argv)
     Action::Result takeoff_result = action.takeoff();
     check_action_result(takeoff_result, "Takeoff failed");
     std::cout << "In Air..." << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(3));
+    sleep_for(milliseconds(3000));
 
     // Start offboard test
     offboard.set_position_ned({0, 0, -5, 0});
 
     const Offboard::Result offboard_start_result = offboard.start();
     check_offboard_result(offboard_start_result, "Offboard start failed:");
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    sleep_for(milliseconds(2000));
 
     std::cout << "PITCH DOWN to the north" << std::endl;
 #ifdef USE_VELOCITY_CONTROL
     offboard.set_velocity_ned({10, 0, 0, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    sleep_for(milliseconds(2000));
     offboard.set_velocity_ned({0, 0, 0, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    sleep_for(milliseconds(1000));
 #else
     offboard.set_position_ned({10, 0, -5, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    sleep_for(milliseconds(5000));
 #endif
 
     std::cout << "PITCH UP to the south" << std::endl;
 #ifdef USE_VELOCITY_CONTROL
     offboard.set_velocity_ned({-10, 0, 0, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    sleep_for(milliseconds(2000));
     offboard.set_velocity_ned({0, 0, 0, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    sleep_for(milliseconds(1000));
 #else
     offboard.set_position_ned({0, 0, -5, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    sleep_for(milliseconds(5000));
 #endif
 
     std::cout << "ROLL RIGHT to the east" << std::endl;
 #ifdef USE_VELOCITY_CONTROL
     offboard.set_velocity_ned({0, 10, 0, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    sleep_for(milliseconds(2000));
     offboard.set_velocity_ned({0, 0, 0, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    sleep_for(milliseconds(1000));
 #else
     offboard.set_position_ned({0, 10, -5, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    sleep_for(milliseconds(5000));
 #endif
 
     std::cout << "ROLL LEFT to the west" << std::endl;
 #ifdef USE_VELOCITY_CONTROL
     offboard.set_velocity_ned({0, -10, 0, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    sleep_for(milliseconds(2000));
     offboard.set_velocity_ned({0, 0, 0, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    sleep_for(milliseconds(1000));
 #else
     offboard.set_position_ned({0, 0, -5, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    sleep_for(milliseconds(5000));
 #endif
 
     std::cout << "YAW CLOCKWISE" << std::endl;
     offboard.set_velocity_ned({0, 0, 0, 90});
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    sleep_for(milliseconds(100));
     offboard.set_velocity_ned({0, 0, 0, 180});
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    sleep_for(milliseconds(2000));
 
     std::cout << "YAW COUNTER-CLOCKWISE" << std::endl;
     offboard.set_velocity_ned({0, 0, 0, 90});
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    sleep_for(milliseconds(100));
     offboard.set_velocity_ned({0, 0, 0, 0});
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    sleep_for(milliseconds(2000));
 
     const Offboard::Result offboard_stop_result = offboard.stop();
     check_offboard_result(offboard_stop_result, "Offboard start failed:");
@@ -163,11 +227,14 @@ int main(int argc, char** argv)
     // Check if vehicle is still in air
     while (telemetry.in_air()) {
         std::cout << "Vehicle is landing..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        sleep_for(milliseconds(1000));
     }
 
     std::cout << "Landed!" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    return EXIT_SUCCESS;
+    // Stop recording
+    is_recording = false;
+    recording_thread.join();
+
+    return 0;
 }
